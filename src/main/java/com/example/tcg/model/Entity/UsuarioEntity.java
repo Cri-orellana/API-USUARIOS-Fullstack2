b1e3fa30dd87c619dfa17d1ccf0f27c74a5bd7d2
Cristian Orellana <cristianj.orellanaa@gmail.com>
2025-11-26 20:13:44 -0300

    dockerfile

--- src/main/java/com/example/tcg/model/Entity/UsuarioEntity.java
+++ src/main/java/com/example/tcg/model/Entity/UsuarioEntity.java
@@ -1,22 +1,61 @@
 package com.example.tcg.model.Entity;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 
 @Entity
-@Data
 public class UsuarioEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Integer id;
 
     private String usuarioNombre;
     private String correo;
     private String password;
     private Boolean terminos;
+
+    public UsuarioEntity() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsuarioNombre() {
+        return usuarioNombre;
+    }
+
+    public void setUsuarioNombre(String usuarioNombre) {
+        this.usuarioNombre = usuarioNombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getTerminos() {
+        return terminos;
+    }
+
+    public void setTerminos(Boolean terminos) {
+        this.terminos = terminos;
+    }
 }
--- src/main/java/com/example/tcg/model/Usuario.java
+++ src/main/java/com/example/tcg/model/Usuario.java
@@ -1,18 +1,10 @@
 package com.example.tcg.model;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Data;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Usuario {
     @Id
@@ -24,4 +16,54 @@ public class Usuario {
     private String password;
     private Boolean terminos;
 
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String usuarioNombre, String correo, String password, Boolean terminos) {
+        this.id = id;
+        this.usuarioNombre = usuarioNombre;
+        this.correo = correo;
+        this.password = password;
+        this.terminos = terminos;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsuarioNombre() {
+        return usuarioNombre;
+    }
+
+    public void setUsuarioNombre(String usuarioNombre) {
+        this.usuarioNombre = usuarioNombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getTerminos() {
+        return terminos;
+    }
+
+    public void setTerminos(Boolean terminos) {
+        this.terminos = terminos;
+    }
 }
